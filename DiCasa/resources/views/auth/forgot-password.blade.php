7f9f2093f33ad15c269f6d54472bcf5c9004f763
Yuri Lago <yuri.l.philomeno@gmail.com>
2025-05-27 22:16:52 -0300

    Atualizando view

--- DiCasa/resources/views/auth/forgot-password.blade.php
+++ DiCasa/resources/views/auth/forgot-password.blade.php
@@ -1,7 +1,6 @@
 <x-guest-layout>
-
-    <div class="mb-4 text-sm text-black">
-        {{ __('Esqueceu sua senha? Sem problemas. Basta nos informar seu endereço de e-mail e enviaremos um link para redefinição de senha que permitirá que você escolha uma nova.') }}
+    <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
+        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
     </div>
 
     <!-- Session Status -->
@@ -13,15 +12,13 @@
         <!-- Email Address -->
         <div>
             <x-input-label for="email" :value="__('Email')" />
-
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus style="background-color: #d9d9d9;" />
-
+            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
         <div class="flex items-center justify-end mt-4">
             <x-primary-button>
-                {{ __('Enviar link para o email') }}
+                {{ __('Email Password Reset Link') }}
             </x-primary-button>
         </div>
     </form>
--- DiCasa/resources/views/components/dropdown.blade.php
+++ DiCasa/resources/views/components/dropdown.blade.php
@@ -1,10 +1,9 @@
-
 @props([
-'align' => 'right',
-'width' => '48',
-'contentClasses' => 'py-1 bg-white dark:bg-[#FFCC6D]'
+    'align' => 'right',
+    'width' => '48',
+    'contentClasses' => 'py-1 bg-white ',
+    'active' => false,
 ])
-
 
 @php
 $alignmentClasses = match ($align) {
@@ -13,28 +12,34 @@ $alignmentClasses = match ($align) {
     default => 'ltr:origin-top-right rtl:origin-top-left end-0',
 };
 
-$width = match ($width) {
+$widthClass = match ($width) {
     '48' => 'w-48',
     default => $width,
 };
+
+$triggerClasses = $active
+    ? 'bg-gray-100 dark:bg-gray-800 text-white route-' . Route::currentRouteName()
+    : 'text-black';
 @endphp
 
 <div class="relative" x-data="{ open: false }" @click.outside="open = false" @close.stop="open = false">
-    <div @click="open = ! open">
+    {{-- TRIGGER SLOT --}}
+    <div @click="open = ! open" class="{{ $triggerClasses }}">
         {{ $trigger }}
     </div>
 
+    {{-- DROPDOWN CONTENT --}}
     <div x-show="open"
-            x-transition:enter="transition ease-out duration-200"
-            x-transition:enter-start="opacity-0 scale-95"
-            x-transition:enter-end="opacity-100 scale-100"
-            x-transition:leave="transition ease-in duration-75"
-            x-transition:leave-start="opacity-100 scale-100"
-            x-transition:leave-end="opacity-0 scale-95"
-            class="absolute z-50 mt-2 {{ $width }} rounded-md shadow-lg {{ $alignmentClasses }}"
-            style="display: none;"
-            @click="open = false">
-        <div class="rounded-md ring-1 ring-black ring-opacity-5 {{ $contentClasses }}">
+         x-transition:enter="transition ease-out duration-200"
+         x-transition:enter-start="opacity-0 scale-95"
+         x-transition:enter-end="opacity-100 scale-100"
+         x-transition:leave="transition ease-in duration-75"
+         x-transition:leave-start="opacity-100 scale-100"
+         x-transition:leave-end="opacity-0 scale-95"
+         class="absolute z-50 mt-2 {{ $widthClass }} rounded-md shadow-lg {{ $alignmentClasses }}"
+         style="display: none;"
+         @click="open = false">
+        <div class="rounded-md ring-1 bg-white ring-black ring-opacity-5 {{ $contentClasses }}">
             {{ $content }}
         </div>
     </div>
