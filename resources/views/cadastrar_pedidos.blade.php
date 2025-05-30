69056982b2fe0404df7af13b9f996d9c35ccfe0d
Ymlp20 <yuri.l.philomeno@gmail.com>
2025-05-30 15:45:24 -0300

    Atualizando o layout

--- resources/views/cadastrar_pedidos.blade.php
+++ resources/views/cadastrar_pedidos.blade.php
@@ -10,25 +10,48 @@
   <div class="container">
     <h1>Pedido</h1>
 
-    <div id="clientes-container">
-      <div class="cliente">
-        <h2>Cliente</h2>
-        <label>Nome</label>
-        <input type="text" placeholder="Digite o nome" />
-
-        <label>Itens</label>
-        <div class="itens">
-          <select><option>Comida 1</option><option>Comida 2</option></select>
-          <select><option>P</option><option>M</option><option>G</option></select>
-          <select><option>1</option><option>2</option><option>3</option></select>
-        </div>
-
-        <label>Obs</label>
-        <input type="text" placeholder="Observações" />
+    <div class="corpo">
+      <div class="linha1">
+        <label for="nome">Nome</label>
+        <input type="text" name="nome">
       </div>
+      <div class="linha2">
+        <label for="nome">Item</label>
+        <select name="itens" class="itens">
+          <option value="Frango">Frango</option>
+          <option value="Mandioca">Mandioca</option>
+          <option value="Linguiça">Linguiça</option>
+        </select>
+        <select name="Tamanho" class="tamanho">
+          <option value="pequena">P</option>
+          <option value="media">M</option>
+          <option value="grande">G</option>
+        </select>
+        <input type="number" name="quantidade" value="1" class="quantidade">
+        <button class="adicionarPrato">Adicionar</button>
+      </div>
+      <div class="linha3">
+        <div class="pedidos">
+           <table>
+            <thead>
+              <tr>
+                <th class="qtdT">Qtd</th>
+                <th class="pratoT">Prato</th>
+                <th class="tamanhoT">Tamanho</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>2x</td> 
+                <td>Macarrão sem molho</td>
+                <td>Média</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+      
     </div>
-
-    <button class="botao" onclick="adicionarPessoa()">+ cliente</button>
 
     <div class="resumo">
       <h3>Resumo</h3>
