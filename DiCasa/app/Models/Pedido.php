9049f70bc9ba594923d12bbb4c6bec49c951ba63
Murilo Bioni Caruso <murilob.programador@gmail.com>
2025-06-04 13:44:13 -0300

    Atualizando o banco de dados

--- DiCasa/app/Models/Pedido.php
+++ DiCasa/app/Models/Pedido.php
@@ -2,19 +2,10 @@
 
 namespace App\Models;
 
-<<<<<<< HEAD
-=======
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-
->>>>>>> f3dc1799141b22dd243d5bd2fdf0b212cf7e3675
 use Illuminate\Database\Eloquent\Model;
 
 class Pedido extends Model
 {
-<<<<<<< HEAD
-=======
-
->>>>>>> f3dc1799141b22dd243d5bd2fdf0b212cf7e3675
     protected $fillable =[
             'total_preco',
             'taxa_entrega',
