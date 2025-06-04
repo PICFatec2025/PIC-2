f3dc1799141b22dd243d5bd2fdf0b212cf7e3675
Ymlp20 <yuri.l.philomeno@gmail.com>
2025-06-04 11:41:04 -0300

    Resolução do conflito na model pedidos

--- DiCasa/app/Models/Pedido.php
+++ DiCasa/app/Models/Pedido.php
@@ -2,15 +2,13 @@
 
 namespace App\Models;
 
-<<<<<<< HEAD
-=======
 use Illuminate\Database\Eloquent\Factories\HasFactory;
->>>>>>> 5a9bdc5dcce23c3512fe3f63dd48103d065a809a
+
 use Illuminate\Database\Eloquent\Model;
 
 class Pedido extends Model
 {
-<<<<<<< HEAD
+
     protected $fillable =[
             'total_preco',
             'taxa_entrega',
@@ -24,27 +22,4 @@ class Pedido extends Model
         return $this->belongsTo(Cliente::class);
     }
 }
-=======
-    use HasFactory;
 
-    protected $fillable = [
-        'nome_cliente',
-        'pedido',
-        'hora',
-        'tipo',
-        'produzindo',
-        'pronto',
-        'entregue',
-        'finalizado',
-        'finalizado_em'
-    ];
-
-    protected $casts = [
-        'produzindo' => 'boolean',
-        'pronto' => 'boolean',
-        'entregue' => 'boolean',
-        'finalizado' => 'boolean',
-        'finalizado_em' => 'datetime'
-    ];
-}
->>>>>>> 5a9bdc5dcce23c3512fe3f63dd48103d065a809a
