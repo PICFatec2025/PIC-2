4939fefde4fefae3033afd2eb6513e300269268f
Murilo Bioni Caruso <murilob.programador@gmail.com>
2025-06-08 16:22:17 -0300

    Adicionei o campo de modo de entrega no Pedidos

---
+++ DiCasa/database/migrations/2025_06_08_191249_criar_campo_modo_retirada_pedidos.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table(
+            'pedidos',
+            function (Blueprint $table) {
+                $table->enum('modo_retirada',['entrega','localmente'])->default('localmente')->after('forma_pagamento');
+            }
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+                Schema::table(
+            'pedidos',
+            function (Blueprint $table) {
+                $table->dropColumn('modo_retirada');
+            }
+        );
+    }
+};
